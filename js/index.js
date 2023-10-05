15db6026c606725a598bc987cd6652e5761e24b5
kaue-cardoso <kauekauada@hotmail.com>
2023-10-05 20:10:05 -0300

    05/10

--- js/index.js
+++ js/index.js
@@ -26,27 +26,31 @@ function login() {
     var loginEle = document.getElementById("email").value;
     var passEle = document.getElementById("password").value;
     var mensagemErro = document.getElementById("mensagem-erro");
-    document.getElementById("login").style.background = "rgb(255, 255, 255)";
-    setTimeout(() => { document.getElementById("login").style.background = "rgb(235, 28, 28)"; }, 100);
+    // document.getElementById("login").style.background = "rgb(255, 255, 255)";
+    // setTimeout(() => { document.getElementById("login").style.background = "rgb(235, 28, 28)"; }, 100);
 
     if (passEle.trim() == "" || loginEle.trim() == "") {
         //caso deixem em branco
         document.getElementById("password").style.borderColor = "red";
         document.getElementById("email").style.borderColor = "red";
-        mensagemErro.textContent = "Preencha os campos.";
+        document.getElementById("label_email").style.color = "red";
+        document.getElementById("label_password").style.color = "red";
+        
+        alert("Preencha os campos.");
     } else {
         // retira do local storage
         var emailRegistrado = "kaue@gmail.com";
-        var senhaRegistrada = 123;
+        var senhaRegistrada = '123';
         // var emailRegistrado = localStorage.getItem("emailRegistrado");
         // var senhaRegistrada = localStorage.getItem("senhaRegistrada");
 
         if (loginEle === emailRegistrado && passEle === senhaRegistrada) {
             // volta ao menu
-            window.location.href = "/pages/sign_up.html";
+            window.location.href = "/ProjetoPIM/index.html";
         } else {
             // Credenciais inválidas
-            mensagemErro.textContent = "Credenciais inválidas.";
+            
+            alert("Credenciais inválidas.");
         }
     }
 }
