0708ab98fde077549c65438d9e6b4a1df235b104
kisalto <joshuabinotto@hotmail.com>
2023-10-06 13:24:17 -0300

    v1.1.3

--- ProjetoPIM/contatos.js
+++ ProjetoPIM/contatos.js
@@ -1,58 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.getElementById("myModal");
-    const a = document.getElementById("joshua");
-    const addModal = document.getElementById("addModal");
-    const adicionar = document.getElementById("adicionar");
+  const modal = document.getElementById("myModal");
+  const addModal = document.getElementById("addModal");
+  const adicionar = document.getElementById("adicionar");
+  const content = document.getElementById("modalContent");
 
-    const span = Array.from(document.getElementsByClassName("close"));
+  const span = Array.from(document.getElementsByClassName("close"));
 
-    span.forEach(close => {
-        close.addEventListener("click", function() {
-            addModal.style.display = "none";
-            modal.style.display = "none";
-        });
+  var open = Array.from(document.getElementsByClassName("openModal"));
+
+  open.forEach((openModal) => {
+    openModal.addEventListener("click", function (event) {
+      modal.style.display = "block";
+      
+      document.getElementById("mudarNome").innerHTML = oi;
     });
+  });
 
-    adicionar.addEventListener("click", function(event) {
-        event.preventDefault();
-        addModal.style.display = "block";
+  span.forEach((close) => {
+    close.addEventListener("click", function () {
+      addModal.style.display = "none";
+      modal.style.display = "none";
     });
+  });
 
-    a.addEventListener("click", function (event) {
-        event.preventDefault();
-        modal.style.display = "block";
-    });
+  adicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+    addModal.style.display = "block";
+  });
 
-    window.addEventListener("click", function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        } else if (event.target == addModal) {
-            addModal.style.display = "none";
-        }
-    });
-    
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    } else if (event.target == addModal) {
+      addModal.style.display = "none";
+    }
+  });
 });
 
-function armazenar (){
+function armazenar() {
+  // session storage
 
-    // session storage
-    
-    var nome = document.getElementById("nome").value;
-    var telefone = document.getElementById("telefone").value;
+  var nome = document.getElementById("nome").value;
+  var telefone = document.getElementById("telefone").value;
 
-    const table = document.getElementById("table");
-        sessionStorage.setItem("nome", nome);
-        sessionStorage.setItem("telefone", telefone);
-        
-        const tr = table.insertRow();
-        
-        var td = tr.insertCell();
-        td.appendChild(document.createTextNode(nome));
-        
-        td = tr.insertCell();
-        td.appendChild(document.createTextNode(telefone));
-    
-    // content change
-    
+  const table = document.getElementById("table");
+
+  localStorage.setItem("nome", nome);
+  localStorage.setItem("telefone", telefone);
+  const tr = table.insertRow();
+
+  var td = tr.insertCell();
+  td.appendChild(document.createTextNode(nome));
+
+  td = tr.insertCell();
+  td.appendChild(document.createTextNode(telefone));
+
+  // content change
 }
-    
