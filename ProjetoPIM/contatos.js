02cc4a3a25fa76b478e4552aa4278bbde99795c0
Joshua Binotto <133135094+kisalto@users.noreply.github.com>
2023-10-10 20:53:51 -0300

    Update contatos.js

--- ProjetoPIM/contatos.js
+++ ProjetoPIM/contatos.js
@@ -1,32 +1,10 @@
+const modal = document.getElementById("myModal");
+const addModal = document.getElementById("addModal");
+const adicionar = document.getElementById("adicionar");
+
+const span = Array.from(document.getElementsByClassName("close"));
+
 document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("myModal");
-  const addModal = document.getElementById("addModal");
-  const adicionar = document.getElementById("adicionar");
-
-  const span = Array.from(document.getElementsByClassName("close"));
-
-  var open = Array.from(document.getElementsByClassName("openModal"));
-
-  open.forEach((openModal) => {
-    openModal.addEventListener("click", function (event) {
-      modal.style.display = "block";
-      
-      let titulo = open[0].title;
-
-      var dado = sessionStorage.getItem(titulo);
-
-      if (dado == titulo) {
-        document.getElementById("mudarNome").innerHTML = dado;
-      }
-    });
-  });
-
-  span.forEach((close) => {
-    close.addEventListener("click", function () {
-      addModal.style.display = "none";
-      modal.style.display = "none";
-    });
-  });
 
   adicionar.addEventListener("click", function (event) {
     event.preventDefault();
@@ -42,25 +20,52 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function armazenar() {
-  // session storage
 
+function armazenar() {
+  var pos = 0;
+  // session storage
   var nome = document.getElementById("nome").value;
   var telefone = document.getElementById("telefone").value;
 
   const table = document.getElementById("table");
 
+  pos += 1;
+
+  sessionStorage.setItem(pos, pos);
   sessionStorage.setItem(nome, nome);
   sessionStorage.setItem(telefone, telefone);
   const tr = table.insertRow();
-
   var td = tr.insertCell();
-  td.appendChild(document.createTextNode(nome));
 
-  td = tr.insertCell();
-  td.appendChild(document.createTextNode(telefone));
+  var link = document.createElement("a");
+  var texto = document.createTextNode(nome);
+  var a = document.createAttribute("id");
+  a.value = pos;
 
-  // content change
+  link.appendChild(texto);
+  link.href = "#";
+  link.className = "openModal";
+  link.setAttributeNode(a);
+  
+  td.appendChild(link);
+  criarModal();
+}
+
+function criarModal() {
+  var open = Array.from(document.getElementsByClassName("openModal"));
+
+  open.forEach((openModal) => {
+    openModal.addEventListener("click", function (event) {
+      modal.style.display = "block";
+    });
+  });
+
+  span.forEach((close) => {
+    close.addEventListener("click", function () {
+      addModal.style.display = "none";
+      modal.style.display = "none";
+    });
+  });
 }
 
 function excluir () {
