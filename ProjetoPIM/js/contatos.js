517422e774349ddbfafdf610cc10172354aa51cb
Joshua Binotto <133135094+kisalto@users.noreply.github.com>
2023-10-23 19:17:27 -0300

    Update contatos.js

--- ProjetoPIM/js/contatos.js
+++ ProjetoPIM/js/contatos.js
@@ -1,33 +1,9 @@
+const modal = document.getElementById("myModal");
+const addModal = document.getElementById("addModal");
+const adicionar = document.getElementById("adicionar");
+const excluir = document.getElementById("excluir")
+
 document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("myModal");
-  const addModal = document.getElementById("addModal");
-  const adicionar = document.getElementById("adicionar");
-
-  const span = Array.from(document.getElementsByClassName("closeModal"));
-
-  var open = Array.from(document.getElementsByClassName("openModal"));
-
-  open.forEach((openModal) => {
-    openModal.addEventListener("click", function (event) {
-      modal.style.display = "block";
-      
-      let titulo = open[0].title;
-
-      var dado = sessionStorage.getItem(titulo);
-
-      if (dado == titulo) {
-        document.getElementById("mudarNome").innerHTML = dado;
-      }
-    });
-  });
-
-  span.forEach((closeModal) => {
-    closeModal.addEventListener("click", function () {
-      addModal.style.display = "none";
-      modal.style.display = "none";
-    });
-  });
-
   adicionar.addEventListener("click", function (event) {
     event.preventDefault();
     addModal.style.display = "block";
@@ -38,27 +14,123 @@ document.addEventListener("DOMContentLoaded", function () {
       modal.style.display = "none";
     } else if (event.target == addModal) {
       addModal.style.display = "none";
+
+      document.getElementById("nome").value = null;
+      document.getElementById("telefone").value = null;
     }
   });
+
+  if (sessionStorage.length != 0) {
+    pos = sessionStorage.length-1;
+    var pessoa = [];
+    for (let i = 0; i < sessionStorage.length; i++) {
+      if (i > 0) {
+        pessoa = sessionStorage.getItem(i);
+
+        const stringPessoa = JSON.parse(pessoa);
+
+        criarTabela(stringPessoa);
+      }
+    }
+  }
 });
+
+var pos = 0;
+const table = document.getElementById("table");
 
 function armazenar() {
   // session storage
-
   var nome = document.getElementById("nome").value;
   var telefone = document.getElementById("telefone").value;
 
-  const table = document.getElementById("table");
+  pos += 1;
 
-  sessionStorage.setItem(nome, nome);
-  sessionStorage.setItem(telefone, telefone);
-  const tr = table.insertRow();
+  var pessoa = {
+    nome: nome,
+    telefone: telefone,
+  };
 
-  var td = tr.insertCell();
-  td.appendChild(document.createTextNode(nome));
+  criarTabela(pessoa);
 
-  td = tr.insertCell();
-  td.appendChild(document.createTextNode(telefone));
+  const stringPessoa = JSON.stringify(pessoa);
 
-  // content change
+  sessionStorage.setItem(pos, stringPessoa);
+
+}
+
+function criarTabela(pessoas) {
+
+  var tr = table.insertRow();
+  tr.id = "row " + pos;
+
+  var td = tr.insertCell(0);
+
+  var link = document.createElement("a");
+
+  var texto = document.createTextNode(pessoas.nome);
+
+  // <a href="#" class="openModal" id="pos"><p>nome</p></a>
+  link.appendChild(texto);
+  link.href = "#";
+  link.className = "openModal";
+  link.id = pos;
+
+  td.appendChild(link);
+  criarModal();
+
+  addModal.style.display = "none";
+  modal.style.display = "none";
+}
+
+function criarModal() {
+  var open = Array.from(document.getElementsByClassName("openModal"));
+  const span = Array.from(document.getElementsByClassName("fechar"));
+
+  open.forEach((openModal) => {
+    openModal.addEventListener("click", function () {
+      modal.style.display = "block";
+
+      const stringPessoa = sessionStorage.getItem(openModal.id);
+
+      const pessoa = JSON.parse(stringPessoa);
+
+      // document.getElementById("mudarNome").value
+      document.getElementById("mudarNome").innerHTML = pessoa.nome;
+      document.getElementById("mudarTelefone").innerHTML = pessoa.telefone;
+    });
+
+    excluir.addEventListener("click", function () {
+      document.getElementById("row " + openModal.id).remove();
+
+      modal.style.display = "none";
+
+      sessionStorage.clear();
+    });
+  });
+
+  span.forEach((close) => {
+    close.addEventListener("click", function () {
+      addModal.style.display = "none";
+      modal.style.display = "none";
+
+      document.getElementById("nome").value = null;
+      document.getElementById("telefone").value = null;
+    });
+  });
+}
+
+function busca(){
+  let barra = document.getElementById("procura").value;
+  barra = barra.toLowerCase();
+  var vetor = document.getElementsByClassName("openModal");
+
+  for (let i = 0; i < vetor.length; i++) {
+    if (!vetor[i].innerHTML.toLowerCase().includes(barra)) {
+      const linha = document.getElementById("row " + (i+1));
+      linha.style.display = "none";
+    } else {
+      const linha = document.getElementById("row " + (i+1));
+      linha.style.display = "block";
+    }
+  }
 }
