0938424440f7232c2b02693a6cf284183c81887c
Joshua Binotto <133135094+kisalto@users.noreply.github.com>
2023-11-09 08:12:03 -0300

    Update processos.js

--- ProjetoPIM/js/processos.js
+++ ProjetoPIM/js/processos.js
@@ -1,29 +1,33 @@
+const body = document.querySelector("body");
+sideBar = body.querySelector(".sidebar");
+toggle = body.querySelector(".toggle");
+searchBtn = body.querySelector(".search-box");
+let content = body.querySelector('.content')
 
-    document.addEventListener("DOMContentLoaded", function () {
-      const cardContainer = document.getElementById("card-container");
-      const addCardButton = document.getElementById("adicionar");
-  
-      // Function to create a new card
-      function createCard() {
-          const card = document.createElement("div");
-          card.classList.add("card");
-  
-          const cardContent = document.createElement("div");
-          cardContent.classList.add("card-content");
-          cardContent.setAttribute("contenteditable", "true");
-          cardContent.textContent = "Click to edit...";
-  
-          card.appendChild(cardContent);
-  
-          // Add an event listener to make the card editable
-          cardContent.addEventListener("click", function () {
-              this.focus();
-          });
-  
-          // Append the card to the container
-          cardContainer.appendChild(card);
-      }
-  
-      // Add event listener to the "Add Card" button
-      addCardButton.addEventListener("click", createCard);
+toggle.addEventListener("click", () => {
+  sideBar.classList.toggle("close");
+  let arrClasses = [];
+  for (let i = 0; i < sideBar.classList.length; i++) {
+    arrClasses.push(sideBar.classList[i]);
+  }
+
+  if (arrClasses.indexOf("close") > -1) {
+    content.style["margin-left"] = "70px"
+  } else {
+    content.style["margin-left"] = "220px"
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var openModalBtn = document.getElementById("openModalBtn");
+  var modal = document.getElementById("myModal");
+
+  openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block";
   });
+
+  var closeModalBtn = document.getElementById("closemod");
+  closeModalBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+  });
+});
