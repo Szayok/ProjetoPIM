9e0e3e134c38c701044382065275f840fa0e4e3c
Ana Julia Nunes <144181881+anajununes@users.noreply.github.com>
2023-11-13 00:40:38 -0300

    Create despesas.js

---
+++ ProjetoPIM/js/despesas.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var registrar = document.getElementById('registrar');
+    if (registrar) {
+        registrar.addEventListener('click', function() {
+            alert('Registrado!');
+        });
+    }
+    const pagarButton = document.getElementById('botaoPagar');
+    if (pagarButton) {
+        pagarButton.addEventListener('click', function() {
+            alert('Despesa liquidada!');
+        });
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    const salvarButton = document.getElementById("salvar");
+    salvarButton.addEventListener("click", function () {
+      const modalMensagem = new bootstrap.Modal(document.getElementById("modalMensagem"), {
+        backdrop: "static",
+      });
+      modalMensagem.show();
+    });
+  });
